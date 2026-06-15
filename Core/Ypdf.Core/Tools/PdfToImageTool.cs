48f285fe6ac151930f11642b29f4866a89727b7f
yakovypg <yasha-pmp@mail.ru>
2026-06-15 15:13:48 +0300

    Remove the ability to extract images from multiple files

--- Core/Ypdf.Core/Tools/PdfToImageTool.cs
+++ Core/Ypdf.Core/Tools/PdfToImageTool.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Ypdf.Core.Config;
 using Ypdf.Core.Extensions;
 using Ypdf.Core.Runtime.Logging;
@@ -8,7 +6,7 @@ using Ypdf.Core.Runtime.Python;
 
 namespace Ypdf.Core.Tools;
 
-public class PdfToImageTool : PythonTool, IMultipleInputTool, IMultipleOutputTool
+public class PdfToImageTool : PythonTool, IMultipleOutputTool
 {
     public PdfToImageTool(
         int extractedImagesLimit = 0,
@@ -24,8 +22,7 @@ public class PdfToImageTool : PythonTool, IMultipleInputTool, IMultipleOutputToo
 
     protected override IEnumerable<PythonPackage> VirtualEnvironmentPackages =>
     [
-        new("PyMuPDF", "1.27.2.2"),
-        new("tqdm", "4.67.3")
+        new("PyMuPDF", "1.27.2.2")
     ];
 
     public override void Execute(string inputPath, string outputPath)
@@ -35,37 +32,13 @@ public class PdfToImageTool : PythonTool, IMultipleInputTool, IMultipleOutputToo
         DefaultExceptions.ThrowIfFileNotExists(inputPath, nameof(inputPath));
         DefaultExceptions.ThrowIfDirectoryNotExists(outputPath, nameof(outputPath));
 
-        Execute(new string[] { inputPath }, outputPath);
-    }
+        inputPath = inputPath.Quoted();
+        outputPath = outputPath.Quoted();
 
-    public void Execute(IEnumerable<string> inputPaths, string outputPath)
-    {
-        ExtendedArgumentNullException.ThrowIfNull(inputPaths, nameof(inputPaths));
-        ExtendedArgumentNullException.ThrowIfNull(outputPath, nameof(outputPath));
-        DefaultExceptions.ThrowIfContainsNotExistingFile(inputPaths, nameof(inputPaths));
-        DefaultExceptions.ThrowIfDirectoryNotExists(outputPath, nameof(outputPath));
-
-        if (string.IsNullOrEmpty(outputPath))
-            outputPath = "\"\"";
-        else
-            outputPath = outputPath.Quoted();
-
-        inputPaths = inputPaths.Select(t => t.Quoted());
-
-        string paths = string.Join(" ", inputPaths);
         string imageExtractorPath = PythonScriptPaths.ImageExtractor.Quoted();
-        string args = $"{imageExtractorPath} -l {ExtractedImagesLimit} -o {outputPath} -i {paths}";
+        string args = $"{imageExtractorPath} -i {inputPath} -o {outputPath} -l {ExtractedImagesLimit}";
 
         PythonExecutor executor = CreateDefaultPythonExecutor();
-        executor.ErrorDataVerifier = t => !string.IsNullOrEmpty(t);
-
-        executor.ErrorDataConverter = t =>
-        {
-            return t is not null && t.Contains(OutputMarks.PdfToImageToolPages, StringComparison.Ordinal)
-                ? $"\n{t}"
-                : t;
-        };
-
         executor.Execute(args);
     }
 }
